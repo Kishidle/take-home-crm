9b05542cd341a6424eb239ddcb96bd72f6282992
Ramon Arca <ramonteodoroarca@gmail.com>
2025-08-19 20:17:52 +0800

    Add detailed return message
    Also print elapsed time, change dictionary build logic

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,9 @@ from io import StringIO
 from pyisemail import is_email
 from dotenv import load_dotenv
 from hubspot import HubSpot
-from hubspot.crm.contacts import BatchInputSimplePublicObjectBatchInputForCreate as Batch
+from hubspot.crm.contacts import (
+    BatchInputSimplePublicObjectBatchInputForCreate as Batch,
+)
 from hubspot.crm.contacts.exceptions import ApiException
 import faker
 import csv
@@ -14,6 +16,7 @@ import requests
 import os
 import json
 import time
+
 f = faker.Faker()
 app = FastAPI()
 origins = ["*"]
@@ -47,10 +50,11 @@ async def upload(file: UploadFile = File(...)):
 
     reader = csv.DictReader(buffer)
 
-    contacts_to_import=[]
+    contacts_to_import = []
     contacts = []
     column_names = reader.fieldnames
-    
+    error_count = 0
+    duplicate_count = 0
     for index, row in enumerate(reader):
         print(f"Importing contact {index + 1}...")
         new_contact = {}
@@ -66,22 +70,46 @@ async def upload(file: UploadFile = File(...)):
             phonenumbers.parse(new_contact["phone"], None)
         ):
             print(f"Found an invalid phone number: {new_contact['phone']}")
+            error_count += 1
             continue
         if not new_contact["company"]:
             print("Missing required field: company")
+            error_count += 1
             continue
         if not new_contact["name"]:
             print("Missing required field: name")
-            continue   
+            error_count += 1
+            continue
+        if not new_contact["lead_type"]:
+            # add default lead_type
+            new_contact["lead_type"] = "NEW_BUSINESS"
+        if not new_contact["lead_label"]:
+            # add default lead_label
+            new_contact["lead_label"] = "HOT"
+
+        # check if email already exists as a contact in Hubspot
         try:
-            email_response = api_client.crm.contacts.basic_api.get_by_id(contact_id=new_contact["email"], id_property="email", archived=False)
+            email_response = api_client.crm.contacts.basic_api.get_by_id(
+                contact_id=new_contact["email"], id_property="email", archived=False
+            )
             if email_response:
                 print(f"Email already exists: {new_contact['email']} as a contact")
-            continue;
+                duplicate_count += 1
+                error_count += 1
+            continue
         except ApiException as e:
             # email not found, do nothing
             pass
-            
+
+        # check if email already exists in contacts list
+        if [
+            contact for contact in contacts if contact["email"] == new_contact["email"]
+        ]:
+            print(f"Email already exists: {new_contact['email']} in contacts list")
+            duplicate_count += 1
+            error_count += 1
+            continue
+
         # split name into first_name, last_name
         name = new_contact["name"].split(" ")
         if len(name) > 2:
@@ -106,11 +134,8 @@ async def upload(file: UploadFile = File(...)):
                 "email": new_contact["email"],
                 "company": new_contact["company"],
                 "phone": new_contact["phone"],
-
-            }
-            
+            },
         }
-
 
         contacts.append(new_contact)
         contacts_to_import.append(contact)
@@ -118,18 +143,19 @@ async def upload(file: UploadFile = File(...)):
     # batch create of contacts
     batch_input = Batch(inputs=contacts_to_import)
     try:
-        
+
         response = api_client.crm.contacts.batch_api.create(batch_input)
-        #check if create is successful
+
         print(f"Successfully created {len(response.results)} contacts")
     except ApiException as e:
-        print("Exception when calling batch_api->create: %s\n" % e)
-        return {"message": "Error creating contacts"}
-    
-    # build dictionary of all contacts by email
-    print("Building contact dictionary...")
-    contact_dict = build_dict(contacts, key_func=lambda d: d["email"])
+        print("An error occurred during contact batch creation: %s\n" % e)
+        return {"message": "Error creating contacts", "status": "fail"}
 
+    # build dictionary of all contacts by email
+    # the assumption is that all emails are unique
+    print("Building contact dictionary...")
+    #contact_dict = build_dict(contacts, key_func=lambda d: d["email"])
+    contact_dict = { contact["email"]: contact for contact in contacts }
     leads = []
     print("Creating batch of leads...")
 
@@ -141,36 +167,55 @@ async def upload(file: UploadFile = File(...)):
     # create leads
     for result in hub_contact_dict:
         contact_info = contact_dict.get(result["properties"]["email"])
-        
+
         if contact_info:
             leads.append(
                 {
-                    "associations": [{
-                        "types": [
-                            {
-                                "associationCategory": "HUBSPOT_DEFINED",
-                                "associationTypeId": 578
-                            }
-                        ],
-                        "to": {
-                            "id": result["id"],
+                    "associations": [
+                        {
+                            "types": [
+                                {
+                                    "associationCategory": "HUBSPOT_DEFINED",
+                                    "associationTypeId": 578,
+                                }
+                            ],
+                            "to": {
+                                "id": result["id"],
+                            },
                         }
-                    }],
+                    ],
                     "properties": {
-                        "hs_lead_name": contact_info["firstname"] + " " + contact_info["lastname"],
+                        "hs_lead_name": contact_info["firstname"]
+                        + " "
+                        + contact_info["lastname"],
                         "hs_lead_type": contact_info["lead_type"],
-                        "hs_lead_label": contact_info["lead_label"]
-                    }
+                        "hs_lead_label": contact_info["lead_label"],
+                    },
                 }
             )
         else:
             print(f"No contact found for email: {result.properties.email}")
 
-    lead_response = requests.request("POST", url, data=json.dumps({"inputs": leads}), headers=headers)
-    print("Leads created successfully")
+    try:
+        lead_response = requests.request(
+            "POST", url, data=json.dumps({"inputs": leads}), headers=headers
+        )
+        print(f"Successfully created {len(lead_response.json()['results'])} leads")
+    except Exception as e:
+        print("An error occurred during lead batch creation: %s\n" % e)
+        return {"message": "Error creating leads", "status": "fail"}
+
     end_time = time.perf_counter()
+    elapsed_time = end_time - start_time
     print(f"Contact and lead creation took {(end_time - start_time):.4f} seconds")
-    return {"message": "Contact and leads created successfully on Hubspot"}
+    return {
+        "message": "Contact and leads created successfully on Hubspot",
+        "runtime": elapsed_time,
+        "totalContacts": len(contacts),
+        "totalErrors": error_count,
+        "duplicatesSkipped": duplicate_count,
+        "status": "success",
+    }
 
 
 def generate_random_phone_number():
@@ -178,15 +223,15 @@ def generate_random_phone_number():
     for _ in range(10):
         phone_number += str(r.randint(0, 9))
     return phone_number
-def build_dict(seq, key_func):
-    return dict((key_func(d), dict(d, index=index)) for (index, d) in enumerate(seq))
 
 @app.post("/generate")
 async def generate():
     file_name = "generated.csv"
     with open(file_name, "w", newline=""):
         writer = csv.writer(open(file_name, "w", newline=""))
-        writer.writerow(["name", "email", "company", "phone", "lead_type", "lead_label"])
+        writer.writerow(
+            ["name", "email", "company", "phone", "lead_type", "lead_label"]
+        )
         for _ in range(100):
             name = f.name()
             email = f.email()
